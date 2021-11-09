f87bdce493fa2a8eb3d2946992c87d849b15b244
raylon <raylon.f@hotmail.com>
2021-11-09 17:07:27 -0300

    Implantation class Person, Phone, PhoneType enum

--- src/main/java/one/digitalinovation/cadperson/entity/Person.java
+++ src/main/java/one/digitalinovation/cadperson/entity/Person.java
@@ -6,24 +6,33 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
 
+    @Column(nullable = false)
     private String firstName;
 
+    @Column(nullable = false)
     private String lastName;
 
+    @Column(nullable = false, unique = true)
     private String cpf;
 
     private LocalDate birthDate;
 
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones;
 }
--- src/main/java/one/digitalinovation/cadperson/entity/Phone.java
+++ src/main/java/one/digitalinovation/cadperson/entity/Phone.java
@@ -6,15 +6,23 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import one.digitalinovation.cadperson.enums.PhoneType;
 
+import javax.persistence.*;
+
+@Entity
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Phone {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
 
+    @Column(nullable = true)
     private String number;
 }
