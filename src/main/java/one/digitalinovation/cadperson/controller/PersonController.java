c9a3190972ea3d4aae707cda07b87a44ec015320
raylon <raylon.f@hotmail.com>
2021-11-11 16:03:59 -0300

    Developed listAll method

--- src/main/java/one/digitalinovation/cadperson/controller/PersonController.java
+++ src/main/java/one/digitalinovation/cadperson/controller/PersonController.java
@@ -2,13 +2,13 @@ package one.digitalinovation.cadperson.controller;
 
 import one.digitalinovation.cadperson.dto.request.PersonDTO;
 import one.digitalinovation.cadperson.dto.response.MessageResponseDTO;
-import one.digitalinovation.cadperson.entity.Person;
 import one.digitalinovation.cadperson.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -25,6 +25,12 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return personService.createPerson(personDTO);
+
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
 
     }
 
